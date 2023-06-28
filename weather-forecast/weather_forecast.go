60ce3caf625368c1e03a4a096f80e0e527ca2033
Jason <jason.rgd@gmail.com>
2023-06-28 20:48:45 +1000

    finished the weathe forecast exercise

--- weather-forecast/weather_forecast.go
+++ weather-forecast/weather_forecast.go
@@ -1,8 +1,12 @@
+// Package weather provides ability to print the city and weather condition.
 package weather
 
+// CurrentCondition is the name of the weather condition.
 var CurrentCondition string
+// CurrentLocation is the name of the city.
 var CurrentLocation string
 
+//Forecast takes in the city and condition and returns the forecast string.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
