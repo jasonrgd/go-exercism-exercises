45cc333c0089ab1e266e4bd87501aee0440ead93
Jason <jason.rgd@gmail.com>
2023-06-29 23:58:54 +1000

    1 test fix remaining

--- party-robot/party_robot.go
+++ party-robot/party_robot.go
@@ -2,15 +2,15 @@ package partyrobot
 
 // Welcome greets a person by name.
 func Welcome(name string) string {
-	panic("Please implement the Welcome function")
+	return "Welcome to my party, "+name+"!"
 }
 
 // HappyBirthday wishes happy birthday to the birthday person and exclaims their age.
 func HappyBirthday(name string, age int) string {
-	panic("Please implement the HappyBirthday function")
+	return "Happy birthday "+name+"! You are now "+string(age)+" years old!"
 }
 
 // AssignTable assigns a table to each guest.
 func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
-	panic("Please implement the AssignTable function")
+	return ""
 }
